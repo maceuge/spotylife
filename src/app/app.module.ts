20a97d14193e541de44fdc9bfad5e1c14064b16b
unknown <maceuge@icloud.com>
2018-07-05 12:34:06 -0300

    Creacion de componentes iniciales para estructura web. Carga de Ico.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FootbarComponent } from './components/shared/footbar/footbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    AboutComponent,
+    NavbarComponent,
+    FootbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/footbar/footbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footbar',
+  templateUrl: './footbar.component.html',
+  styles: []
+})
+export class FootbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
