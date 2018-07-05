4c7c62c0357209e6517b9f6d8c76c102661245f0
unknown <maceuge@icloud.com>
2018-07-05 17:52:50 -0300

    Creacion de Diseño inicial, viculacion de Routs con los Links, app .ico y Google Material Fonts

--- src/app/components/shared/footbar/footbar.component.ts
+++ src/app/components/shared/footbar/footbar.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footbar',
   templateUrl: './footbar.component.html',
-  styles: []
+  styles: ['./footbar.component.css']
 })
 export class FootbarComponent implements OnInit {
+
+  copyright = new Date();
 
   constructor() { }
 
